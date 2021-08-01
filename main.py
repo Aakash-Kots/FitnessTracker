12cdf1178ecb972edead580b6024e5ab2b422c37
Epicgamer75 <kots.aakash@gmail.com>
2021-08-01 18:22:49 +0530

    added some further functionality;

--- main.py
+++ main.py
@@ -1,36 +1,124 @@
 from tkinter import *
 from exercises import exercises
+import mysql.connector as mysql
+from tkcalendar import Calendar
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+from tkinter.ttk import *
+from dotenv import load_dotenv
+
+load_dotenv()
 master = Tk()
-print('Hello world!')
-master.geometry("200x200")
-master.configure(bg='blue')
 
-
-def show():
-    label1.config(text=clicked.get() + ' - ' + str(reps.get()) + ' reps')
-
-
+master.geometry("600x350")
+master.configure(bg='turquoise')
 options = exercises
 
+
+
+def connectDB():
+    con = mysql.connect(host='localhost', user='root', password=os.getenv('password'), database='FitnessDB')
+    custor = con.cursor()
+    return custor
+
+
+
+def save(reps, exercise, date, weight):
+    label1.config(text=clicked.get() + ' - ' + str(repCounter.get()) + ' reps')
+    date = date
+    Exercise = exercise
+    Repetitions = reps
+    weight = weight
+    con = mysql.connect(host='localhost', user='root', password=os.getenv('password'), database='FitnessDB')
+    custor = con.cursor()
+    custor.execute("insert into FitnessTB (Exercise,Weight, Reps, Date) values('" + str(Exercise) + "','" + weight + "','" + str(Repetitions) + "','" + str(date) + "')")
+    custor.execute("commit")
+    con.close()
+
+
+def DoPlot(var, weight):
+    var = var.get()
+    con = mysql.connect(host='localhost', user='root', password=os.getenv('password'), database='FitnessDB')
+    custor = con.cursor()
+    custor.execute(f"SELECT * FROM FitnessTB WHERE Exercise='{var}'")
+    value = custor.fetchall()
+    x = []
+    y = []
+
+    for data in value:
+        if int(data[2]) == int(weight):
+            x.append(data[4])
+            y.append(int(data[3]))
+
+
+    # x = value[0]
+    # y = value[1]
+    plt.plot(x, y)
+    plt.title(var)
+    plt.xlabel('Weight')
+    plt.ylabel('Reps')
+    plt.show()
+    con.close()
+
+
+def showAllData():
+    con = mysql.connect(host='localhost', user='root', password=os.getenv('password'), database='FitnessDB')
+    custor = con.cursor()
+    custor.execute("SELECT * FROM FitnessTB")
+    Exercise = []
+    Weight = []
+    Repetitions = []
+    Date = []
+    values = custor.fetchall()
+    for value in values:
+        Weight.append(value[2])
+        Exercise.append(value[1])
+        Repetitions.append(value[3])
+        Date.append(value[4])
+    d = {'Exercise':Exercise,'Weight':Weight,'Repetitions':Repetitions,'Data':Date}
+    df = pd.DataFrame(data=d)
+    file_name = 'AllData.xlsx'
+    df.to_excel(file_name)
+
+    os.system("open /Users/aakashkothari/Desktop/FitnessTrackerProject/AllData.xlsx")
+    print(values)
+
+
+def openPlot():
+    plot = Toplevel()
+    plot.title('Plotting Page')
+    dropVar2 = StringVar()
+    drop2 = OptionMenu(plot, dropVar2, *options).grid(column=0)
+    weightToPlot = Spinbox(plot,from_=0,to_=1000,width=4)
+    weightToPlot.grid(row=1,column=0)
+    drawGraph = Button(plot, text='Plot Graph', command=lambda: DoPlot(dropVar2, weightToPlot.get()))
+    drawGraph.grid(row=2, column=0)
+
+
+
 clicked = StringVar()
-
+btn = Button(master, text='Open Plot Page', command=openPlot).grid(column=3, row=2)
 clicked.set("Exercise")
-
+cal = Calendar(master,selectmode='day',year=2021,month=8,day=1)
+cal.grid(row=1, padx=20)
 reps = IntVar()
 
-
 drop = OptionMenu(master,clicked,*options)
-drop.grid(padx=(10,10))
+drop.grid(row=0,padx=(10,10))
+showAll = Button(master,text='Show all data', command=showAllData).grid(row=1, column=1)
 
+weightVal = Spinbox(master,from_=0, to_=1000, width=2)
+weightVal.grid(row=2,column=0)
 
 repCounter = Spinbox(master,from_=0, to_=99, width=2)
-repCounter.grid(row=0,column=1)
+repCounter.grid(row=3,column=0)
 
-button = Button(master,text='Click',command=show,bg="blue").grid(pady=(10,10))
+button = Button(master,text='Click',command=lambda: save(repCounter.get(),clicked.get(),cal.get_date(),weightVal.get())).grid(pady=(10,10))
+# button2 = Button(master,text='Plot', command=newWindow).grid()
+label1 = Label(master,text="Select exercise,reps and date")
 
-label1 = Label(master,text="NA")
-
-label1.grid(pady=(10,10))
+label1.grid(pady=(10,10), padx=(10,10))
 
 mainloop()
 
